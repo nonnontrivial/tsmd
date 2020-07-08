dd50276f87d00bbcd285af19f24a21d9a58111a8
Kevin Donahue <nonnontrivial@gmail.com>
2020-07-08 18:13:28 -0400

    format

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,12 @@
+use std::collections::HashMap;
+
 pub struct Parser {}
 
 impl Parser {
-  pub fn new() -> Self {
-    Self {}
-  }
+    pub fn new() -> Self {
+        Self {}
+    }
+    pub fn collect_interface_map(&self, contents: &str) -> HashMap<&str, HashMap<&str, &str>> {
+        unimplemented!()
+    }
 }
